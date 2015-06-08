10f06ccc2a85f10bf1abb8e963008e04767bca25
S-ngularity <s.ngularity@gmail.com>
2015-06-08 16:12:15 -0300

    regex para ano sendo gerado, import para Jython
    Jython adicionado nos Resources, crio um interpretador de Python no
    ConsultaValorDAO e rodo um programa em python que gera as regex pra uma
    range de números (conseguidas no Consulta) - seria bom fazer tudo menos
    a chamada de função (regex_for_rage.__call__(new
    PyInteger(consulta.getDataInicio()), new
    PyInteger(consulta.getDataFim()));) estáticamente pra não ter que
    reiterpretar o programa toda vez (deixa lenta a primeira busca).
    
    BUG: pelo jeito tem algum bug na página quando essa consulta retorna
    vazia, porque quando coloco na página uma range 2015-2017, por exemplo,
    ele mantem a primeira página (em vez de ficar vazio) e some com a
    barrinha de páginas. Não é problema da regex (99% ctz) pq eu testei a
    consulta direto no PostgreSQL usando o retorno da função (que to
    printando no System.out) funcionou como esperado.

--- src/java/dadosGovernamentais/dao/ConsultaValorDAO.java
+++ src/java/dadosGovernamentais/dao/ConsultaValorDAO.java
@@ -9,6 +9,9 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.python.util.PythonInterpreter;
+import org.python.core.*; 
+
 
 public class ConsultaValorDAO 
 {
@@ -74,7 +77,10 @@ public class ConsultaValorDAO
                 stmt.setInt(1, 2);
                 stmt.setInt(2, 0);
                 stmt.setString(3, consulta.getTipoLicitacao());
-                stmt.setString(4, "20[0-1][1-5]");
+               //stmt.setString(4, "20[0-1][1-5]");
+				String regex = new String(getAnoRegex(consulta));
+				System.out.println(">>>>> " + regex + " <<<<<");
+				stmt.setString(4, regex);
                 stmt.setString(5, "[0-9][0-9][0-9][0-9].[0-9][0-9]");
                 rs = stmt.executeQuery();
                 if(rs.next())
@@ -94,4 +100,130 @@ public class ConsultaValorDAO
         }
         return nrows;            
     }
+	
+	
+	
+	
+	private String getAnoRegex(ConsultaValorInputBean consulta)
+	{
+		PythonInterpreter pyInterp = new PythonInterpreter();
+
+		pyInterp.exec(""
+			+ "import math\n" +
+			"\n" +
+			"# coding=utf8\n" +
+			"\n" +
+			"#  Split range to ranges that has its unique pattern.\n" +
+			"#  Example for 12-345:\n" +
+			"#\n" +
+			"#  12- 19: 1[2-9]\n" +
+			"#  20- 99: [2-9]\\d\n" +
+			"# 100-299: [1-2]\\d{2}\n" +
+			"# 300-339: 3[0-3]\\d\n" +
+			"# 340-345: 34[0-5]\n" +
+			"\n" +
+			"\n" +
+			"def bounded_regex_for_range(min_, max_):\n" +
+			"    return r'\\b({})\\b'.format(regex_for_range(min_, max_))\n" +
+			"\n" +
+			"\n" +
+			"def regex_for_range(min_, max_):\n" +
+			"    \"\"\"\n" +
+			"    > regex_for_range(12, 345)\n" +
+			"    '1[2-9]|[2-9]\\d|[1-2]\\d{2}|3[0-3]\\d|34[0-5]'\n" +
+			"    \"\"\"\n" +
+			"    positive_subpatterns = []\n" +
+			"    negative_subpatterns = []\n" +
+			"\n" +
+			"    if min_ < 0:\n" +
+			"        min__ = 1\n" +
+			"        if max_ < 0:\n" +
+			"            min__ = abs(max_)\n" +
+			"        max__ = abs(min_)\n" +
+			"\n" +
+			"        negative_subpatterns = split_to_patterns(min__, max__)\n" +
+			"        min_ = 0\n" +
+			"\n" +
+			"    if max_ >= 0:\n" +
+			"        positive_subpatterns = split_to_patterns(min_, max_)    \n" +
+			"\n" +
+			"    negative_only_subpatterns = ['-' + val for val in negative_subpatterns if val not in positive_subpatterns]\n" +
+			"    positive_only_subpatterns = [val for val in positive_subpatterns if val not in negative_subpatterns]\n" +
+			"    intersected_subpatterns = ['-?' + val for val in negative_subpatterns if val in positive_subpatterns]\n" +
+			"\n" +
+			"    subpatterns = negative_only_subpatterns + intersected_subpatterns + positive_only_subpatterns\n" +
+			"    return '|'.join(subpatterns)\n" +
+			"\n" +
+			"\n" +
+			"def split_to_patterns(min_, max_):\n" +
+			"    subpatterns = []\n" +
+			"\n" +
+			"    start = min_\n" +
+			"    for stop in split_to_ranges(min_, max_):\n" +
+			"        subpatterns.append(range_to_pattern(start, stop))\n" +
+			"        start = stop + 1\n" +
+			"\n" +
+			"    return subpatterns\n" +
+			"\n" +
+			"\n" +
+			"def split_to_ranges(min_, max_):\n" +
+			"    stops = {max_}\n" +
+			"\n" +
+			"    nines_count = 1\n" +
+			"    stop = fill_by_nines(min_, nines_count)\n" +
+			"    while min_ <= stop < max_:\n" +
+			"        stops.add(stop)\n" +
+			"\n" +
+			"        nines_count += 1\n" +
+			"        stop = fill_by_nines(min_, nines_count)\n" +
+			"\n" +
+			"    zeros_count = 1\n" +
+			"    stop = fill_by_zeros(max_ + 1, zeros_count) - 1\n" +
+			"    while min_ < stop <= max_:\n" +
+			"        stops.add(stop)\n" +
+			"\n" +
+			"        zeros_count += 1\n" +
+			"        stop = fill_by_zeros(max_ + 1, zeros_count) - 1\n" +
+			"\n" +
+			"    stops = list(stops)\n" +
+			"    stops.sort()\n" +
+			"\n" +
+			"    return stops\n" +
+			"\n" +
+			"\n" +
+			"def fill_by_nines(integer, nines_count):\n" +
+			"    return int(str(integer)[:-nines_count] + '9' * nines_count)\n" +
+			"\n" +
+			"\n" +
+			"def fill_by_zeros(integer, zeros_count):\n" +
+			"    return integer - integer % 10 ** zeros_count\n" +
+			"\n" +
+			"\n" +
+			"def range_to_pattern(start, stop):\n" +
+			"    pattern = ''\n" +
+			"    any_digit_count = 0\n" +
+			"\n" +
+			"    for start_digit, stop_digit in zip(str(start), str(stop)):\n" +
+			"        if start_digit == stop_digit:\n" +
+			"            pattern += start_digit\n" +
+			"        elif start_digit != '0' or stop_digit != '9':\n" +
+			"            pattern += '[{}-{}]'.format(start_digit, stop_digit)\n" +
+			"        else:\n" +
+			"            any_digit_count += 1\n" +
+			"\n" +
+			"    if any_digit_count:\n" +
+			"        pattern += r'\\d'\n" +
+			"\n" +
+			"    if any_digit_count > 1:\n" +
+			"        pattern += '{{{}}}'.format(any_digit_count)\n" +
+			"\n" +
+			"    return pattern");
+		// execute a function that takes a string and returns a string
+		PyObject regex_for_rage = pyInterp.get("regex_for_range");
+		PyObject result = regex_for_rage.__call__(new PyInteger(consulta.getDataInicio()),
+												  new PyInteger(consulta.getDataFim()));
+		String regEx = (String) result.__tojava__(String.class);
+		
+		return regEx;
+	}
 }
