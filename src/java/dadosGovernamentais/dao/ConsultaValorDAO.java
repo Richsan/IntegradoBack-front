85f51f31ed8c6699d749ceabb80dd762a7bdd87a
S-ngularity <s.ngularity@gmail.com>
2015-06-08 23:09:28 -0300

    python algorithm license included

--- src/java/dadosGovernamentais/dao/ConsultaValorDAO.java
+++ src/java/dadosGovernamentais/dao/ConsultaValorDAO.java
@@ -123,6 +123,31 @@ public class ConsultaValorDAO
 		pyInterp = new PythonInterpreter();
 
 		// https://github.com/dimka665/range-regex
+		/*
+			Copyright (c) 2013, Dmitry Voronin
+			All rights reserved.
+
+			Redistribution and use in source and binary forms, with or without
+			modification, are permitted provided that the following conditions are met:
+
+			1. Redistributions of source code must retain the above copyright notice, this
+			list of conditions and the following disclaimer.
+
+			2. Redistributions in binary form must reproduce the above copyright notice,
+			this list of conditions and the following disclaimer in the documentation
+			and/or other materials provided with the distribution.
+
+			THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+			ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+			WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+			DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+			FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+			DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+			SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+			CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+			OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+			OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+		*/
 		pyInterp.exec(""
 			+ "import math\n" +
 			"\n" +
@@ -233,7 +258,7 @@ public class ConsultaValorDAO
 			"        pattern += '{{{}}}'.format(any_digit_count)\n" +
 			"\n" +
 			"    return pattern");
-		// execute a function that takes a string and returns a string
+		
 		regex_for_rage = pyInterp.get("regex_for_range");
 	}
 }
